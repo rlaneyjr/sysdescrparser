d3f3a93ea1d07fed7b78c5a1ac0df77b1a898484
Ricky Laney <rlaneyjr@gmail.com>
2024-07-05 14:46:34 -0400

    Added another FTD

--- sysdescrparser/cisco_ftd.py
+++ sysdescrparser/cisco_ftd.py
@@ -24,9 +24,19 @@ class CiscoFTD(Cisco):
     def parse(self):
         """Parse.
         Cisco FirePOWER FPR-2140 Security Appliance, System Version 2.10(1.175)
+        Cisco Firepower Threat Defense, Version 7.2.5.2 (Build 4), ASA Version 9.18(3)61
         """
         regex = (r'Cisco FirePOWER (\w+-\d+)\s'
                  r'Version (\d.*)')
+        pat = re.compile(regex)
+        res = pat.search(self.raw)
+        if res:
+            self.model = res.group(1)
+            self.version = res.group(2)
+            return self
+
+        regex = (r'Cisco Firepower Threat Defense, Version (\d.*),'
+                 r'ASA Version (\d.*)')
         pat = re.compile(regex)
         res = pat.search(self.raw)
         if res:
