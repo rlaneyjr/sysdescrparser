fd8264b734eea17297df56df2203250619d74cd2
Ricky Laney <rlaneyjr@gmail.com>
2024-07-05 12:13:22 -0400

    Added support for Cisco FTD

--- sysdescrparser/cisco_ftd.py
+++ sysdescrparser/cisco_ftd.py
@@ -7,28 +7,31 @@ from sysdescrparser.cisco import Cisco
 
 
 # pylint: disable=no-member
-class CiscoASA(Cisco):
-    """Class CiscoASA.
+class CiscoFTD(Cisco):
+    """Class CiscoFTD.
 
-    SNMP sysDescr for CiscoASA.
+    SNMP sysDescr for CiscoFTD.
 
     """
 
     def __init__(self, raw):
         """Constructor."""
-        super(CiscoASA, self).__init__(raw)
-        self.os = 'ASA'
+        super(CiscoFTD, self).__init__(raw)
+        self.os = 'FTD'
         self.model = self.UNKNOWN
         self.version = self.UNKNOWN
 
     def parse(self):
-        """Parse."""
-        regex = (r'Cisco Adaptive Security Appliance '
+        """Parse.
+        Cisco FirePOWER FPR-2140 Security Appliance, System Version 2.10(1.175)
+        """
+        regex = (r'Cisco FirePOWER (\w+-\d+)\s'
                  r'Version (\d.*)')
         pat = re.compile(regex)
         res = pat.search(self.raw)
         if res:
-            self.version = res.group(1)
+            self.model = res.group(1)
+            self.version = res.group(2)
             return self
 
         return False
